78e37a40faa0f430960b58a37f1094bad5dba0f5
AlexRuppert <sumpkrautjunkie@googlemail.com>
2014-11-20 17:23:24 +0100

    swagger integration

--- LAPPS-backend/src/main/java/de/rwth/dbis/layers/lapps/Main.java
+++ LAPPS-backend/src/main/java/de/rwth/dbis/layers/lapps/Main.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.net.URI;
 
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.StaticHttpHandler;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+
+import com.wordnik.swagger.jaxrs.config.BeanConfig;
 
 /**
  * Creates and starts a new instance of {@link HttpServer} waiting for an user input.
@@ -23,11 +26,21 @@ public class Main {
   public static HttpServer startServer() {
     // create a resource config that scans for JAX-RS resources and providers
     // in de.rwth.dbis.layers.lapps package
-    final ResourceConfig rc = new ResourceConfig().packages("de.rwth.dbis.layers.lapps");
+    // add com.wordnik.swagger.jersey.listing for swagger support
+    String[] packages = {"de.rwth.dbis.layers.lapps", "com.wordnik.swagger.jersey.listing"};
+    final ResourceConfig rc = new ResourceConfig().packages(packages);
 
+    // Configure swagger
+    BeanConfig config = new BeanConfig();
+    config.setResourcePackage("de.rwth.dbis.layers.lapps");
+    config.setVersion("1.0.0");
+    config.setScan(true);
     // create and start a new instance of grizzly http server
     // exposing the Jersey application at BASE_URI
-    return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    // Host static web page for swagger ui
+    server.getServerConfiguration().addHttpHandler(new StaticHttpHandler("src/main/webapp/"), "/");
+    return server;
   }
 
   /**
