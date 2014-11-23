76e909d5783108ad43121565b00624bc0b4e687f
Martin <martin.hug@rwth-aachen.de>
2014-11-23 11:09:27 +0100

    swagger documentation for user ressource (/users/{id}), LOGGER for exception, LAPPS-69

--- LAPPS-backend/src/main/java/de/rwth/dbis/layers/lapps/resource/UserResource.java
+++ LAPPS-backend/src/main/java/de/rwth/dbis/layers/lapps/resource/UserResource.java
@@ -1,5 +1,7 @@
 package de.rwth.dbis.layers.lapps.resource;
 
+import java.util.logging.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,17 +11,23 @@ import javax.ws.rs.core.Response;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
 
 import de.rwth.dbis.layers.lapps.domain.UserFacade;
 import de.rwth.dbis.layers.lapps.entity.UserEntity;
 
 /**
- * User resource (exposed at "users/{id}" path).
+ * User resource (exposed at "/users" path).
  */
-@Path("users/{id}")
+@Path("/users")
+@Api(value = "/users", description = "User ressource")
 public class UserResource {
 
   private static UserFacade userFacade = new UserFacade();
+  private final static Logger LOGGER = Logger.getLogger(UserResource.class.getName());
 
   /**
    * 
@@ -31,7 +39,10 @@ public class UserResource {
    * 
    */
   @GET
+  @Path("/{id}")
   @Produces(MediaType.APPLICATION_JSON)
+  @ApiOperation(value = "Find user by ID", response = UserEntity.class)
+  @ApiResponses(value = {@ApiResponse(code = 404, message = "User not found")})
   public Response getUser(@PathParam("id") int id) {
     UserEntity user = userFacade.find(id);
     if (user == null)
@@ -40,7 +51,7 @@ public class UserResource {
       ObjectMapper mapper = new ObjectMapper();
       return Response.status(200).entity(mapper.writeValueAsBytes(user)).build();
     } catch (JsonProcessingException e) {
-      // e.printStackTrace(); // TODO have a look at the exception handling
+      LOGGER.warning(e.getMessage());
       return Response.status(500).build();
     }
   }
