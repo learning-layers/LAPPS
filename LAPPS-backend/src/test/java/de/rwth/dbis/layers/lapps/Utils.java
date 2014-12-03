0426aa6a636bcdcea2ff540fba12c31a52e6ed58
AlexRuppert <sumpkrautjunkie@googlemail.com>
2014-12-03 17:26:44 +0100

    Helper to generate random app entities

--- LAPPS-backend/src/test/java/de/rwth/dbis/layers/lapps/Utils.java
+++ LAPPS-backend/src/test/java/de/rwth/dbis/layers/lapps/Utils.java
@@ -1,7 +1,10 @@
 package de.rwth.dbis.layers.lapps;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Random;
+
+import org.junit.Test;
 
 import de.rwth.dbis.layers.lapps.entity.AppEntity;
 import de.rwth.dbis.layers.lapps.entity.AppInstanceEntity;
@@ -32,5 +35,314 @@ public class Utils {
     AppEntity app = new AppEntity("dummy app");
     app.addInstance(instance);
     return app;
+  }
+
+  @Test
+  public void mockupAppGenerator() {
+    for (int i = 0; i < 1; i++) {
+      // System.out.println(MockupAppGenerator.getRandomName());
+      // System.out.println(MockupAppGenerator.getRandomIpsumBlock());
+      System.out.println(MockupAppGenerator.getRandomText(70, 10));
+    }
+  }
+
+  /**
+   * Generates mockup app instances
+   *
+   */
+  public static class MockupAppGenerator {
+    private final static String[] names = new String[] {"Sun", "Jupiter", "Saturn", "Uranus",
+        "Neptune", "Earth", "Venus", "Mars", "Ganymede", "Titan", "Mercury", "Callisto", "Io",
+        "Moon", "Europa", "Triton", "Pluto", "Eris", "Titania", "Rhea", "Oberon", "Iapetus",
+        "Makemake", "Haumea", "Charon", "Umbriel", "Ariel", "Dione", "Quaoar", "Tethys", "Sedna",
+        "Ceres", "Orcus", "Salacia", "Varda", "Dysnomia", "Varuna", "Ixion", "Chaos", "Pallas",
+        "Vesta", "Enceladus", "Miranda", "Huya", "Hygiea", "Proteus", "Mimas", "Hiʻiaka", "Vanth",
+        "Ilmarë", "Nereid", "Interamnia", "Europa", "Davida", "Sylvia", "Actaea", "Cybele", "Juno",
+        "Hyperion", "Eunomia", "Camilla", "Euphrosyne", "Psyche", "Chariklo", "Sila", "Bamberga",
+        "Patientia", "Chiron", "Thisbe", "Hektor", "Ceto", "Herculina", "Doris", "Nunam",
+        "Eugenia", "Phoebe", "Amphitrite", "Bienor", "Deucalion", "Diotima", "Egeria", "Fortuna",
+        "Aurora", "Iris", "Daphne", "Themis", "Alauda", "Larissa", "Ursula", "Hermione", "Palma",
+        "Metis", "Nemesis", "Hebe", "Pholus", "Bertha", "Freia", "Elektra", "Rhadamanthus",
+        "Janus", "Aletheia", "Galatea", "Teharonhiawako", "Typhon", "Lachesis", "Winchester",
+        "Hilda", "Himalia", "Namaka", "Puck", "Aegle", "Germania", "Prokne", "Stereoskopia",
+        "Amalthea", "Agamemnon", "Kalliope", "Borasisi", "Siegena", "Elpis", "Diomedes", "Gyptis",
+        "Aspasia", "Dioretsa", "Ida", "Comas Solà", "Atlas", "Elatus", "Perdita", "Pan", "Linus",
+        "Ananke", "Telesto", "Phobos", "Paaliaq", "Francisco", "Calypso", "Leda", "Ferdinand",
+        "Margaret", "Medusa", "Romulus", "Ymir", "Trinculo", "Cupid", "Euler", "Adrastea",
+        "Kiviuq", "Herschel", "Tarvos", "Masursky", "Comet Comas Solà", "Bestla", "Kerberos",
+        "Petit-Prince", "Deimos", "Gaspra", "Ijiraq", "Halley's Comet", "Styx", "Mab", "Erriapus",
+        "Misterrogers", "Callirrhoe", "Themisto", "Daphnis", "Remus", "Toutatis", "Šteins",
+        "Phaethon", "Cruithne", "Annefrank", "Pallene", "Polydeuces",
+        "Comet Churyumov–Gerasimenko", "Methone", "Magellan", "Braille", "Geographos", "Apollo",
+        "Astronautica", "Dactyl", "Icarus", "Castalia", "Nyx", "Hypnos", "Aten", "Golevka",
+        "Itokawa", "Aegaeon", "Nereus", "Apophis", "Duende"};
+    private final static String[] nameSuffixes = new String[] {"", "", "", "", "", "", "", "App",
+        "App", "App", "Application", "Tool", "Beta"};
+
+    private final static String loremIpsum =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+            + "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis "
+            + "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+            + "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu "
+            + "fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt "
+            + "in culpa qui officia deserunt mollit anim id est laborum.";
+
+    private final static String[] imageCategories = new String[] {"abstract", "animals",
+        "business", "cats", "city", "food", "nightlife", "fashion", "people", "nature", "sports",
+        "technics", "transport"};
+    private final static int IMAGE_W = 500;
+    private final static int IMAGE_H = 280;
+    private final static int LOGO_W = 150;
+    private final static int LOGO_H = 150;
+
+    /**
+     * Generates a shuffled lorem ipsum textblock.
+     * 
+     * @return a shuffled and capitalized lorem ipsum text
+     */
+    public static String getRandomIpsumBlock() {
+      String[] parts = loremIpsum.split(" ");
+      shuffleArray(parts);
+      String block = combine(parts, " ").toLowerCase();
+      block = capitalize(block) + ".";
+      return block;
+    }
+
+    /**
+     * Capitalizes the first letter after a dot
+     * 
+     * @param input text to capitalize
+     * @return capitalized text
+     */
+    private static String capitalize(String input) {
+      StringBuilder out = new StringBuilder();
+      if (input.length() < 0) {
+        return "";
+      }
+      out.append(Character.toUpperCase(input.charAt(0)));
+      int dotAndSpace = 0;
+      for (int i = 1; i < input.length(); i++) {
+        if (input.charAt(i) == '.') {
+          dotAndSpace = 1;
+          out.append(input.charAt(i));
+        } else if (input.charAt(i) == ' ' && dotAndSpace == 1) {
+          dotAndSpace = 2;
+          out.append(input.charAt(i));
+        } else {
+          if (dotAndSpace == 2) {
+            out.append(Character.toUpperCase(input.charAt(i)));
+            dotAndSpace = 0;
+          } else {
+            out.append(input.charAt(i));
+          }
+
+        }
+      }
+      return out.toString();
+    }
+
+    /**
+     * Joins elements of an array.
+     * 
+     * @param s array to join
+     * @param glue what to put between the elements
+     * @return single string with all elements of the array separated by the glue sequence
+     */
+    private static String combine(String[] s, String glue) {
+      int k = s.length;
+      if (k == 0) {
+        return null;
+      }
+      StringBuilder out = new StringBuilder();
+      out.append(s[0]);
+      for (int x = 1; x < k; ++x) {
+        out.append(glue).append(s[x]);
+      }
+      return out.toString();
+    }
+
+    /**
+     * Fisher–Yates Shuffle
+     * 
+     * @param ar array to shuffle
+     */
+    private static void shuffleArray(String[] ar) {
+      Random rnd = RandomNumberGenerator.getRandom();
+      for (int i = ar.length - 1; i > 0; i--) {
+        int index = rnd.nextInt(i + 1);
+        // Simple swap
+        String a = ar[index];
+        ar[index] = ar[i];
+        ar[i] = a;
+      }
+    }
+
+    /**
+     * Finds all occurrence of a substring in a string
+     * 
+     * @param input base string
+     * @param subString substring to search for
+     * @return list of indices found
+     */
+    private static ArrayList<Integer> findAll(String input, String subString) {
+      int lastIndex = 0;
+      int count = 0;
+      ArrayList<Integer> indices = new ArrayList<Integer>();
+      while (lastIndex != -1) {
+
+        lastIndex = input.indexOf(subString, lastIndex);
+
+        if (lastIndex != -1) {
+          indices.add(lastIndex);
+          lastIndex += subString.length();
+        }
+      }
+      return indices;
+    }
+
+    /**
+     * Inserts a text in a string at a specified index.
+     * 
+     * @param input base text to edit
+     * @param index where to insert
+     * @param toInsert what to insert
+     * @return edited text
+     */
+    private static String insert(String input, int index, String toInsert) {
+      return input.substring(0, index) + toInsert + input.substring(index, input.length());
+    }
+
+    /**
+     * Generates a random name using two internal name arrays and combining random elements of
+     * those.
+     * 
+     * @return a random name
+     */
+    public static String getRandomName() {
+      String name =
+          names[RandomNumberGenerator.getRandomInt(0, names.length - 1)] + " "
+              + nameSuffixes[RandomNumberGenerator.getRandomInt(0, nameSuffixes.length - 1)];
+      return name.trim();
+    }
+
+    /**
+     * Generates a random lorem ipsum text.
+     * 
+     * @param wordCount amount of words required
+     * @param paragraphs amount of empty lines to insert
+     * @return random lorem ipsum text
+     */
+    public static String getRandomText(int wordCount, int paragraphs) {
+      StringBuilder sb = new StringBuilder();
+      int wordSize = 0;
+      String[] parts = new String[] {};
+      int i = 1000;
+
+      while (wordSize < wordCount) {
+        if (i >= parts.length) {
+          i = 0;
+          parts = getRandomIpsumBlock().split(" ");
+        }
+        sb.append(parts[i]);
+        if (wordSize + 1 < wordCount) {
+          sb.append(" ");
+        } else {
+          sb.append(".");
+        }
+        i++;
+        wordSize++;
+      }
+
+      String text = sb.toString();
+      ArrayList<Integer> dots = findAll(text, ".");
+      int insertionOffset = 1;
+      for (int j = 0; j < paragraphs; j++) {
+        if (dots.size() <= 0) {
+          break;
+        }
+        int randomIndex = RandomNumberGenerator.getRandomInt(0, dots.size() - 1);
+        Integer index = dots.get(randomIndex);
+        dots.remove(randomIndex);
+        text = insert(text, index + insertionOffset, "\n\n");
+        insertionOffset += 2;
+      }
+
+      return text;
+    }
+
+    /**
+     * Generates a random image url for an app using lorempixum service.
+     * 
+     * @return an url to a random image
+     */
+    public static String getRandomImageUrl() {
+      return "http://lorempixel.com/" + IMAGE_W + "/" + IMAGE_H + "/"
+          + imageCategories[RandomNumberGenerator.getRandomInt(0, imageCategories.length - 1)];
+    }
+
+    /**
+     * Generates a random image url for an app using lorempixel service.
+     * 
+     * @return an url to a random image
+     */
+    public static String getRandomLogoUrl() {
+      return "http://lorempixel.com/" + LOGO_W + "/" + LOGO_H + "/"
+          + imageCategories[RandomNumberGenerator.getRandomInt(0, imageCategories.length - 1)];
+
+    }
+
+    /**
+     * Generates an array of random image urls
+     * 
+     * @param amount size of the array to generate
+     * @return array with urls of random images
+     */
+    public static String[] getRandomImageUrls(int amount) {
+      String[] res = new String[amount];
+      for (int i = 0; i < amount; i++) {
+        res[i] = getRandomImageUrl();
+      }
+      return res;
+    }
+  }
+
+  /**
+   * Static class to generate a fixed sequence of random numbers (for unit tests)
+   */
+  public static class RandomNumberGenerator {
+    private static RandomNumberGenerator instance = null;
+    private final static int SEED = 9001;
+    private Random rand = new Random(SEED);
+
+    protected RandomNumberGenerator() {
+
+    }
+
+    /**
+     * Generates a random integer value in a range.
+     * 
+     * @param min lowest possible value
+     * @param max highest possible value
+     * @return random value in [min,max]
+     */
+    public static int getRandomInt(int min, int max) {
+      return getInstance().rand.nextInt((max - min) + 1) + min;
+    }
+
+    public static Random getRandom() {
+      return getInstance().rand;
+    }
+
+    /**
+     * Retrieves the singleton instance.
+     * 
+     * @return singleton instance
+     */
+    public static RandomNumberGenerator getInstance() {
+      if (instance == null) {
+        instance = new RandomNumberGenerator();
+      }
+      return instance;
+    }
   }
 }
