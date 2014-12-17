e8e6bb89c316dd54119a21c399c06a358fd0188a
AlexRuppert <sumpkrautjunkie@googlemail.com>
2014-12-17 01:50:56 +0100

    platform selection in navbar first time visitors get a short help message how to change platform platform service keeps track of available and currently selected platform, on first visit detects the platform automatically using the user agent

--- LAPPS-frontend/app/shared/header/headerCtrl.js
+++ LAPPS-frontend/app/shared/header/headerCtrl.js
@@ -5,88 +5,192 @@
  *              user should see links, like admin-area, search etc)
  */
 (function() {
-  angular.module('lappsControllers').controller('headerCtrl',
-          ['$scope', '$location', 'user', function($scope, $location, user) {
-            var SIGN_IN = 'Sign in';
-            var SIGN_OUT = 'Sign out';
-            /**
-             * @field
-             * @type string
-             * @memberOf lapps.lappsControllers.headerCtrl
-             * @description Text displayed on the sign in button.
-             */
-            $scope.loginText = SIGN_IN;
-            /**
-             * @field
-             * @type string
-             * @memberOf lapps.lappsControllers.headerCtrl
-             * @description Current content of the search box.
-             */
-            $scope.searchQuery = '';
+  angular.module('lappsControllers').controller(
+          'headerCtrl',
+          ['$scope', '$location', 'user', 'platform', '$timeout',
+              function($scope, $location, user, platform, $timeout) {
+                var SIGN_IN = 'Sign in';
+                var SIGN_OUT = 'Sign out';
+                /**
+                 * @field
+                 * @type string
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description Text displayed on the sign in button.
+                 */
+                $scope.loginText = SIGN_IN;
+                /**
+                 * @field
+                 * @type string
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description Current content of the search box.
+                 */
+                $scope.searchQuery = '';
 
-            /**
-             * @field
-             * @type boolean
-             * @memberOf lapps.lappsControllers.headerCtrl
-             * @description True if user is signed in. Used to display sign in
-             *              button accordingly to the current state.
-             */
-            $scope.isloggedIn = false;
+                /**
+                 * @field
+                 * @type boolean
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description True if user is signed in. Used to display sign
+                 *              in button accordingly to the current state.
+                 */
+                $scope.isloggedIn = false;
 
-            /**
-             * @function
-             * @memberOf lapps.lappsControllers.headerCtrl
-             * @description Signs the user manually in (when clicking the sign
-             *              in button).
-             */
-            $scope.login = function() {
-              user.signIn();
-            }
-            /**
-             * @function
-             * @memberOf lapps.lappsControllers.headerCtrl
-             * @description Signs the user out (when clicking the sign out
-             *              button).
-             */
-            $scope.logout = function() {
-              user.signOut();
-              $scope.loginCallback(false);
-            }
-            /**
-             * @function
-             * @memberOf lapps.lappsControllers.headerCtrl
-             * @description Redirects to the search page with the given search
-             *              parameters.
-             */
-            $scope.search = function() {
-              $location.path('/search/' + $scope.searchQuery);
-            }
-            /**
-             * @function
-             * @memberOf lapps.lappsControllers.headerCtrl
-             * @param {boolean}
-             *          success True, if the login was successful.
-             * @description Called after a oidc login attempt.
-             */
-            $scope.loginCallback = function(success) {
-              /*
-               * user.signedIn = true; user.data = { sub: 12,
-               * preferred_username: 'JohnDoe', email: 'john@mail1.foobar' };
-               * user.role = 'Developer'; $scope.isloggedIn = true;
-               * $scope.userData = user.data; return;
-               */
+                /**
+                 * @function
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description Signs the user manually in (when clicking the
+                 *              sign in button).
+                 */
+                $scope.login = function() {
+                  user.signIn();
+                }
+                /**
+                 * @function
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description Signs the user out (when clicking the sign out
+                 *              button).
+                 */
+                $scope.logout = function() {
+                  user.signOut();
+                  $scope.loginCallback(false);
+                }
+                /**
+                 * @function
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description Redirects to the search page with the given
+                 *              search parameters.
+                 */
+                $scope.search = function() {
+                  $location.path('/search/' + $scope.searchQuery);
+                }
+                /**
+                 * @function
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @param {boolean}
+                 *          success True, if the login was successful.
+                 * @description Called after a oidc login attempt.
+                 */
+                $scope.loginCallback = function(success) {
+                  /*
+                   * user.signedIn = true; user.data = { sub: 12,
+                   * preferred_username: 'JohnDoe', email: 'john@mail1.foobar' };
+                   * user.role = 'Developer'; $scope.isloggedIn = true;
+                   * $scope.userData = user.data; return;
+                   */
 
-              $scope.isloggedIn = success;
+                  $scope.isloggedIn = success;
 
-              if (!success) {
-              } else {
-                $scope.userData = user.data;
-              }
-              // console.log(user);
+                  if (!success) {
+                  } else {
+                    $scope.userData = user.data;
+                  }
+                  // console.log(user);
+                }
+                /**
+                 * @field
+                 * @type boolean
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description Controls of the help text underneath the navbar
+                 *              is displayed.
+                 */
+                $scope.helperTextVisible = false;
 
-            }
+                /**
+                 * @field
+                 * @type {platform[]}
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description Stores the currently available platforms.
+                 */
+                $scope.platforms = platform.platforms;
+                /**
+                 * @field
+                 * @type {platform}
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description Stores the currently selected platform.
+                 */
+                $scope.currentPlatform = {};
+                platform.selectInitialPlatform();
+                $scope.currentPlatform = platform.currentPlatform;
 
-            user.init($scope.loginCallback);
+                /**
+                 * @function
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description Used to change the selected platform filter
+                 *              (i.e. Linux/Windows etc)
+                 */
+                $scope.changePlatform = function(id) {
+                  $scope.currentPlatform = platform.getPlatformById(id);
+                  platform.changePlatform($scope.currentPlatform);
+                  // needed to wait after rendering of the element is finished
+                  // (we need the new width of the element)
+                  // due to queue 0 is enough waiting time
+                  $timeout($scope.adjustPositions, 0);
+                }
+                /**
+                 * @function
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description Checks if the user visits the page for the first
+                 *              time. If yes, displays a helping notification on
+                 *              how to change the platform filter.
+                 */
+                $scope.displayPlatfromHelper = function() {
+                  if (!window.localStorage['firstVisit']) {
+                    $scope.helperTextVisible = true;
+                    window.localStorage['firstVisit'] = true;
+                    $timeout(function() {
+                      $scope.helperTextVisible = false
+                    }, 15000);
+                  }
+                }
+                /**
+                 * @function
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description Makes sure the arrow-pointer of the help-popup
+                 *              points to the platform selection button.
+                 */
+                $scope.adjustPlatformHelperArrow = function() {
+                  var elem = $('#platform-selector');
+                  var center = elem.offset().left + elem.width() / 2;
+                  if (center > 0) {// not collapsed
+                    $('#help-text > .arrow-up').css('left', center + 'px');
+                  } else {
+                    elem = $('.navbar-header > .navbar-toggle');
+                    center = elem.offset().left + elem.width() / 2;
+                    $('#help-text > .arrow-up').css('left', center + 'px');
+                  }
+                }
+                /**
+                 * @function
+                 * @memberOf lapps.lappsControllers.headerCtrl
+                 * @description Resizes search bar to fit dynamically. Seems
+                 *              like not all current browsers (like Safari for
+                 *              iPhone) support flex.
+                 */
+                $scope.adjustSearchBar = function() {
+                  var elem = $('.navbar-nav > li:last-child');
+                  var right = elem.position().left + elem.width();
+                  if (right > 0) {// not collapsed
+                    $('.search-bar.hidden-xs').css('left', right + 'px');
+                  }
+                }
 
-          }]);
+                user.init($scope.loginCallback);
+
+                // keep helper pointer directly under the platform dropdown
+                $(window).on('resize', function() {
+                  $scope.adjustPlatformHelperArrow();
+                  $scope.adjustSearchBar();
+                });
+
+                $scope.adjustPositions = function() {
+                  $scope.adjustSearchBar();
+                  $scope.adjustPlatformHelperArrow();
+                }
+
+                // when angularjs is ready working on the DOM
+                $scope.$on('$viewContentLoaded', function() {
+                  $scope.adjustPositions();
+                  $scope.displayPlatfromHelper();
+                });
+              }]);
 }).call(this);
---
+++ LAPPS-frontend/app/shared/header/platformService.js
@@ -0,0 +1,160 @@
+﻿/**
+ * @class platform
+ * @memberOf lapps.lappsServices
+ * @description This service handles identification and selection of the current
+ *              device platform.
+ */
+(function() {
+  angular
+          .module('lappsServices')
+          .service(
+                  'platform',
+                  function() {
+                    /**
+                     * @field
+                     * @type {platform[]}
+                     * @memberOf lapps.lappsServices.platform
+                     * @description Stores the currently available platforms.
+                     */
+                    this.platforms = [
+                        {
+                          id: 0,
+                          name: 'iOS',
+                          icon: 'fa-apple',
+                          agentRegEx: /(iPhone|iPad|iPod)/
+                        },
+                        {
+                          id: 1,
+                          name: 'Android',
+                          icon: 'fa-android',
+                          agentRegEx: /Android/
+                        },
+                        {
+                          id: 2,
+                          name: 'Windows Phone',
+                          icon: 'fa-windows',
+                          agentRegEx: /Windows Phone/
+                        },
+                        {
+                          id: 3,
+                          name: 'Web Apps',
+                          icon: 'fa-globe',
+                        },
+                        {
+                          id: 4,
+                          name: 'Windows',
+                          icon: 'fa-windows',
+                          agentRegEx: /(Windows NT|Windows 2000|Windows XP|Windows 7|Windows 8|Windows 10)/
+                        }, {
+                          id: 5,
+                          name: 'Linux',
+                          icon: 'fa-linux',
+                          agentRegEx: /(Linux|X11)/
+                        }, {
+                          id: 6,
+                          name: 'Mac OS X',
+                          icon: 'fa-apple',
+                          agentRegEx: /Mac OS X/
+                        }, {
+                          id: 7,
+                          name: 'All Platforms',
+                          icon: 'fa-circle-o',
+                        }];
+                    /**
+                     * @field
+                     * @type {number}
+                     * @memberOf lapps.lappsServices.platform
+                     * @description Id of the default platform.
+                     */
+                    this.defaultPlatform = 3;
+                    /**
+                     * @field
+                     * @type {platform}
+                     * @memberOf lapps.lappsServices.platform
+                     * @description Stores the currently selected platform.
+                     */
+                    this.currentPlatform = this.platforms[this.defaultPlatform];
+
+                    /**
+                     * @function
+                     * @memberOf lapps.lappsServices.platform
+                     * @type {platform}
+                     * @param {number}
+                     *          id Platform id.
+                     * @description Returns the platform object with the given
+                     *              id.
+                     */
+                    this.getPlatformById = function(id) {
+                      for (var i = 0; i < this.platforms.length; i++) {
+                        if (this.platforms[i].id == id) { return this.platforms[i]; }
+                      }
+                      return -1;
+                    }
+                    /**
+                     * @function
+                     * @type {platform}
+                     * @memberOf lapps.lappsServices.platform
+                     * @description Tries to detect the platform used by the
+                     *              visitor. It checks the user agent string for
+                     *              certain patterns.
+                     */
+                    this.detectPlatform = function() {
+                      var agent = navigator.userAgent;
+
+                      for (var i = 0; i < this.platforms.length; i++) {
+                        if (typeof this.platforms[i].agentRegEx !== 'undefined'
+                                && this.platforms[i].agentRegEx !== null) {
+                          if (this.platforms[i].agentRegEx.test(agent)) { return this.platforms[i]; }
+                        }
+                      }
+                      return this.platforms[this.defaultPlatform];
+                    }
+
+                    /**
+                     * @function
+                     * @memberOf lapps.lappsServices.platform
+                     * @param {platform}
+                     *          platform new platform.
+                     * @description Updates the currently selected platform to a
+                     *              new one. Also stores the new platform in
+                     *              local storage, so it can be loaded from
+                     *              there later.
+                     */
+                    this.changePlatform = function(platform) {
+                      if (typeof platform !== 'number') {
+                        this.currentPlatform = platform;
+                      } else {
+                        this.currentPlatform = this.getPlatformById(platform);
+                      }
+
+                      window.localStorage['platform'] = this.currentPlatform.id;
+                    }
+                    /**
+                     * @function
+                     * @memberOf lapps.lappsServices.platform
+                     * @description Selects an initial platform as the current
+                     *              one, when the page is loaded. If previously
+                     *              visited, the value is loaded from local
+                     *              storage. Otherwise the platform is set based
+                     *              on the user agent detection. If this does
+                     *              not work the default platform is set.
+                     */
+                    this.selectInitialPlatform = function() {
+                      if (window.localStorage['platform']) {
+                        var p = this
+                                .getPlatformById(window.localStorage['platform']);
+                        // nothing found/error
+
+                        if (p < 0) {
+                          this.currentPlatform = this.detectPlatform();
+                          window.localStorage['platform'] = this.currentPlatform.id;
+                        } else {
+                          this.currentPlatform = p;
+                        }
+                      } else {
+                        this.currentPlatform = this.detectPlatform();
+                        window.localStorage['platform'] = this.currentPlatform.id;
+                      }
+                    }
+                  });
+}).call(this);
