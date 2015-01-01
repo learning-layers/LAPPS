545159487288b0381bb1bd9bd03ac5eb19371394
Odgii <odgii.luv@gmail.com>
2015-01-01 17:33:47 +0100

    deleted dropdown directive, changed to select instead.

--- LAPPS-frontend/app/components/uploadAppPage/uploadPageCtrl.js
+++ LAPPS-frontend/app/components/uploadAppPage/uploadPageCtrl.js
@@ -4,92 +4,123 @@
  * @description This controller is responsible for uploading apps
  */
 (function() {
-  angular.module('lappsControllers').controller('uploadPageCtrl',
-          ['$scope', '$http', function($scope, $http) {
+  angular
+          .module('lappsControllers')
+          .controller(
+                  'uploadPageCtrl',
+                  [
+                      '$scope',
+                      '$http',
+                      function($scope, $http) {
 
-            var counter = 0;
+                        $scope.selectedValue = "";
 
-            var currentIndex = 0;
+                        var counter = 0;
 
-            $scope.images = [{
-              id: 0,
-              buttonName: "add",
-              buttonTitle: 'Add'
-            }];
+                        var currentIndex = 0;
 
-            $scope.videolink = "http://www.youtube.com/embed/oHg5SJYRHA0";
+                        $scope.images = [{
+                          id: 0,
+                          buttonName: "add",
+                          buttonTitle: 'Add'
+                        }];
 
-            $scope.tags = " ";
+                        $scope.videolink = "http://www.youtube.com/embed/oHg5SJYRHA0";
 
-            $scope.platforms = [{
-              id: 1,
-              name: 'Windows'
-            }, {
-              id: 2,
-              name: 'Linux'
-            }, {
-              id: 3,
-              name: 'Mac OS'
-            }, {
-              id: 4,
-              name: 'IOS'
-            }, {
-              id: 5,
-              name: 'Android'
-            }, {
-              id: 6,
-              name: 'WindowsPhone'
-            }, {
-              id: 7,
-              name: 'Others'
-            }];
+                        $scope.tags = " ";
 
-            $scope.createNewApp = function() {
+                        this.platforms = [
+                            {
+                              id: 0,
+                              name: 'iOS',
+                              icon: 'fa-apple',
+                              agentRegEx: /(iPhone|iPad|iPod)/
+                            },
+                            {
+                              id: 1,
+                              name: 'Android',
+                              icon: 'fa-android',
+                              agentRegEx: /Android/
+                            },
+                            {
+                              id: 2,
+                              name: 'Windows Phone',
+                              icon: 'fa-windows',
+                              agentRegEx: /Windows Phone/
+                            },
+                            {
+                              id: 3,
+                              name: 'Web Apps',
+                              icon: 'fa-globe',
+                            },
+                            {
+                              id: 4,
+                              name: 'Windows',
+                              icon: 'fa-windows',
+                              agentRegEx: /(Windows NT|Windows 2000|Windows XP|Windows 7|Windows 8|Windows 10)/
+                            }, {
+                              id: 5,
+                              name: 'Linux',
+                              icon: 'fa-linux',
+                              agentRegEx: /(Linux|X11)/
+                            }, {
+                              id: 6,
+                              name: 'Mac OS X',
+                              icon: 'fa-apple',
+                              agentRegEx: /Mac OS X/
+                            }, {
+                              id: 7,
+                              name: 'All Platforms',
+                              icon: 'fa-circle-o',
+                            }];
 
-              console.log($scope.uploadForm.$valid);
-              // $scope.$broadcast('show-errors-check-validity');
+                        $scope.createNewApp = function() {
 
-              if ($scope.uploadForm.$valid) {
-                alert('App Saved');
-                $scope.reset();
-              }
-              /* create json for new app */
-              var data = $scope.newapp;
+                          console.log($scope.uploadForm.$valid);
+                          // $scope.$broadcast('show-errors-check-validity');
 
-            };
+                          if ($scope.uploadForm.$valid) {
+                            alert('App Saved');
+                            $scope.reset();
+                          }
+                          /* create json for new app */
+                          var data = $scope.newapp;
 
-            $scope.reset = function() {
-              $scope.$broadcast('show-errors-reset');
-              $scope.newapp = {
-                name: '',
-                email: ''
-              };
-            }
+                        };
 
-            $scope.addAnotherField = function($event) {
-              alert($event.currentTarget.id);
-              if ($event.currentTarget.name == "add") {
-                counter++;
-                $scope.images.push({
-                  id: counter,
-                  buttonName: 'remove' + counter,
-                  buttonTitle: 'Remove'
-                });
-              } else {
-                $scope.findElementInArray($event.currentTarget.id);
-                $scope.images.splice(currentIndex, 1);
-              }
-              $event.preventDefault();
-            };
+                        $scope.reset = function() {
+                          $scope.$broadcast('show-errors-reset');
+                          $scope.newapp = {
+                            name: '',
+                            email: ''
+                          };
+                        }
 
-            $scope.findElementInArray = function(index) {
-              for (var i = 0; i < $scope.images.length; i++) {
-                if ($scope.images[i].id == index) {
-                  currentIndex = $scope.images.indexOf($scope.images[i]);
-                }
-              }
-            };
+                        $scope.addAnotherField = function($event) {
+                          alert($event.currentTarget.id);
+                          if ($event.currentTarget.name == "add") {
+                            counter++;
+                            $scope.images.push({
+                              id: counter,
+                              buttonName: 'remove' + counter,
+                              buttonTitle: 'Remove'
+                            });
+                          } else {
+                            $scope.findElementInArray($event.currentTarget.id);
+                            $scope.images.splice(currentIndex, 1);
+                          }
+                          $event.preventDefault();
+                        };
 
-          }]);
+                        $scope.findElementInArray = function(index) {
+                          for (var i = 0; i < $scope.images.length; i++) {
+                            if ($scope.images[i].id == index) {
+                              currentIndex = $scope.images
+                                      .indexOf($scope.images[i]);
+                            }
+                          }
+                        };
+
+                      }]);
 
 }).call(this);
